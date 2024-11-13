2498d10a7ade6e197eadc9ece62c1274f9f78287
djonkovalev <djon9489@gmail.com>
2024-11-13 07:12:01 +0700

    Add files via upload

---
+++ 1234.py
@@ -0,0 +1,7 @@
+The_number_of_completed_DZ = 12 # Количество выполненных ДЗ
+print(The_number_of_completed_DZ)
+Number_of_hours_spent = 1.5 # Количество затраченных часов
+print(Number_of_hours_spent)
+Course_name = 'Python' # Название курса
+print(Course_name)
+print (Number_of_hours_spent / The_number_of_completed_DZ)  # Время на одно задание
